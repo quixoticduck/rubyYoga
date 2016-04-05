51475b6f53257d3a531780374ba50468f6d82458
quixoticduck <claraalder@Claras-MacBook.local>
2016-04-05 17:24:12 +0100

    first commit

---
+++ mindthecode.rb
@@ -0,0 +1,5 @@
+adjectives = ["sleepy", "happy", "sideways", "ticklish", "sad", "upside-down", "tartan", "british", "underground", "unicycling"]
+nouns = ["penguin", "elephant", "platypus", "llama", "swordfish", "lilypad", "algae", "sandwich", "cereal", "potato", "beefeater"]
+
+puts (adjectives.sample + " " + nouns.sample)
+#the brackets above are not necessary but they do make it easier to read
